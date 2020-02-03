aca64524823909d1c24cc384abb164bd34ef4060
Georgi Kolishovski <georgi.kolishovski@jax.org>
2020-02-03 12:52:42 -0500

    raised the species and chromosome validations before the query run

--- src/app/controller/cytogenetic_bands_controller.py
+++ src/app/controller/cytogenetic_bands_controller.py
@@ -2,7 +2,9 @@ from flask_restplus import Resource, Namespace, fields, abort
 
 from src.app.service.cytogenetic_bands_service import get_all_bands, get_bands_by_species, \
     get_bands_by_species_and_chromosome
-from src.app.utils.common import check_species_exists
+from src.app.utils.common import check_species_exists, check_chromosome_exists
+
+from src.utils.exceptions import InvalidRequestArgumentValueException
 
 ns = Namespace('bands', description='Returns band information about all cytogenetic bands '
                                     'available in the database, as well as '
@@ -36,7 +38,7 @@ class Bands(Resource):
 
         if not res:
             message = 'No bands data is available currently in the database.'
-            abort(400, message=message)
+            raise InvalidRequestArgumentValueException(400, message)
         return res, 200
 
 
@@ -50,12 +52,14 @@ class BandsBySpeciesId(Resource):
         """
         Returns cytogenetic bands data for the specified species.
         """
-        res = get_bands_by_species(species_id)
+        species_exists = check_species_exists(species_id)
 
-        if not res:
+        if species_exists is False:
             message = f'The species with ID: <{species_id}> is not represented in the database and thus there is ' \
                       f'no associated bands data.'
-            abort(400, message=message)
+            raise InvalidRequestArgumentValueException(400, message)
+
+        res = get_bands_by_species(species_id)
         return res, 200
 
 
@@ -71,16 +75,20 @@ class BandsByChromosome(Resource):
         """
         Returns cytogenetic bands data for the specified species and chromosome.
         """
+
+        # check that the species are available in the application
+        species_exists = check_species_exists(species_id)
+        if species_exists is False:
+            message = f'The species with ID: <{species_id}> is not represented in the database and thus there is ' \
+                      f'no associated cytogenetic bands data.'
+            raise InvalidRequestArgumentValueException(400, message)
+
+        # check that the specified chromosome is valid (for that species)
+        chromosome_exists = check_chromosome_exists(species_id, chromosome)
+        if chromosome_exists is False:
+            message = f'The species with ID: <{species_id}> is represented in the database, ' \
+                      f'but has no associated data for chromosome: <{chromosome}>.'
+            raise InvalidRequestArgumentValueException(400, message)
+
         res = get_bands_by_species_and_chromosome(species_id, chromosome)
-
-        if not res:
-            species_exists = check_species_exists(species_id)
-
-            if species_exists:
-                message = f'The species with ID: <{species_id}> is represented in the database, ' \
-                          f'but has no associated data for chromosome: <{chromosome}>.'
-            else:
-                message = f'The species with ID: <{species_id}> is not represented in the database and thus there is ' \
-                          f'no associated cytogenetic bands data.'
-            abort(400, message=message)
         return res, 200
