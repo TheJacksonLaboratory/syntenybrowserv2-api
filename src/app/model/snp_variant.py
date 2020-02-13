4f816862c40a6ecc77b85b0666e6437d6863b739
Georgi Kolishovski <georgi.kolishovski@jax.org>
2020-02-13 14:27:11 -0500

    small text edit

--- src/app/model/snp_variant.py
+++ src/app/model/snp_variant.py
@@ -3,14 +3,17 @@ from . import BASE
 
 
 class SnpVariant(BASE):
-    """ This class represents the 'cytogenetic_band' table. """
+    """ This class represents the 'snp_Variant' table. """
 
     __tablename__ = 'snp_variant'
-
+    # chromosome
     chr = Column(String, primary_key=True)
+    # chromosome location
     position = Column("pos", Integer, primary_key=True)
     id = Column(String)
+    # reference base - 'A'. 'C', 'T' or 'G'
     ref_base = Column(String)
+    # alternative allele - 'A', 'C', 'T' or 'G'
     alt_allele = Column(String)
     quality = Column(Integer)
     filter = Column(Integer)
