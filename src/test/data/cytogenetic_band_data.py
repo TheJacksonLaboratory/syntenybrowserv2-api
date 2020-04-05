e23ed0a16073ab116a251d42f3cc29e254302721
Govindarajan Ramamoorthy <ramamg@MLI-JGM183.local>
2020-04-05 16:07:49 -0700

    code to test cytogenetic_band

---
+++ src/test/data/cytogenetic_band_data.py
@@ -0,0 +1,9 @@
+"""test data for cytoband"""
+
+CYTOBAND_DATA = [
+    ('cytoband_1', 10090, '1', 'UCSC', 'cytogenetic band', 0, 8840440, 'qA1', 'gpos100'),
+    ('cytoband_2', 10090, '1', 'UCSC', 'cytogenetic band', 8840440, 12278390, 'qA2', 'gneg'),
+    ('cytoband_3', 10090, '1', 'UCSC', 'cytogenetic band', 12278390, 20136559, 'qA3', 'gpos33'),
+    ('cytoband_4', 10090, '1', 'UCSC', 'cytogenetic band', 20136559, 22101102, 'qA4', 'gneg'),
+    ('cytoband_5', 10090, '1', 'UCSC', 'cytogenetic band', 22101102, 30941543, 'qA5', 'gpos100')
+]
